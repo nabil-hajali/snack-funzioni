b513f6cc7a36e016929295a653d249614296b4cc
nabil-hajali <nabil2491@gmail.com>
2026-02-10 17:20:04 +0100

    stampa di una funzione che ha  come obbiettivo di stampare le parole con la stessa iniziale che ho dato

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -4,10 +4,26 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+function sameWord(){
+    const stessaIniziale = [];
+    
+   for(i = 0; i < names.length; i++){
+    if(names[i][0] === 'A'){
+        stessaIniziale.push(names[i]);
+        
+    }
+   }
 
+   return stessaIniziale;
+
+
+
+
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
+console.log(sameWord());
 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
