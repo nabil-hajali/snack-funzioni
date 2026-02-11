52b119d5206b4489a0db2adb47a7fcbbbc692290
nabil-hajali <nabil2491@gmail.com>
2026-02-11 10:38:24 +0100

    complete ex 5

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -4,10 +4,27 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
+function countVowels(str){
 
+    // store the counter for every vowel found
+    let counter = 0
+    let foundVowels = []
+    const vowels = ['a','e','i','o','u']
+
+    for (let i = 0; i < str.length; i++) {
+        const char = str[i]
+        console.log(char);
+        if (vowels.icludes) {
+            
+        }
+        
+    }
+    // return the counter
+    return counter
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
+countVowels(word)
 
-
-//Risultato atteso se si passa 'javascript': 3 (a, a, i)
+// Risultato atteso se si passa 'javascript': 3 (a, a, i)
--- 05_bonus/script.js
+++ 05_bonus/script.js
@@ -9,10 +9,25 @@ const name = 'Mario';
 
 
 // Dichiara la funzione qui.
-
+function greetings(name){
+ const now = new Date()
+ //now.setHours(18)
+const currentHour = now.getHours();
+let message;
+if(currentHour <= 13){
+    message = `Buongiorno ${name}`
+} else if (currentHour > 13 && currentHour <= 17){
+    message = `Buon pomeriggio ${name}`
+} else{
+    message = `Buona sera ${name}`
+}
+ 
+ return message
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+const greet = greetings(name)
 
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
+console.log(greet);
